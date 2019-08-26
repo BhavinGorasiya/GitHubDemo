8a551f8dd8464e94f363fd36467f659235024332
Project-DDB <daberwaladhaval4444@gmail.com>
2019-08-26 16:42:16 +0530

    Add a View

---
+++ Models/form.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubDemo.Models
+{
+    public class form
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
